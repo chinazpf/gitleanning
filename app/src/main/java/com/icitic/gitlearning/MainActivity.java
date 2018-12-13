33ee70238db5859851a8d394355a2a328c8609a6
zhangpengfei <18236015925@163.com>
2018-12-13 15:50:42 +0800

    修改ui‘ git commit -m 修改ui‘

--- app/src/main/java/com/icitic/gitlearning/MainActivity.java
+++ app/src/main/java/com/icitic/gitlearning/MainActivity.java
@@ -5,14 +5,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
+/**
+ * @author zhangpengfei
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LinearLayout llContent = findViewById(R.id.ll_content);
         View view = new View(this);
+        LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) view.getLayoutParams();
+        layoutParams.height = 45;
+        layoutParams.width = 45;
+        view.setLayoutParams(layoutParams);
+        llContent.addView(view);
         Button btn = findViewById(R.id.btn);
         btn.setText("MainActivity");
     }
