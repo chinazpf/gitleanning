0e134cc798142894f0f9c3673099efffb8f16970
zhangpengfei <18236015925@163.com>
2018-12-13 16:09:28 +0800

    fix bug

--- app/src/main/java/com/icitic/gitlearning/MainActivity.java
+++ app/src/main/java/com/icitic/gitlearning/MainActivity.java
@@ -18,12 +18,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         LinearLayout llContent = findViewById(R.id.ll_content);
         View view = new View(this);
+        llContent.addView(view);
         view.setBackgroundResource(R.color.colorPrimary);
         LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) view.getLayoutParams();
         layoutParams.height = 45;
         layoutParams.width = 45;
         view.setLayoutParams(layoutParams);
-        llContent.addView(view);
         Button btn = findViewById(R.id.btn);
         btn.setText("MainActivity");
     }
